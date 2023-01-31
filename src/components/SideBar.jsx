90cf77df93682d1e109871f0144d6a8ca5cf9c48
THOMY NTIKALA LOWE <thomylowe@THOMYs-MacBook-Air.local>
2023-01-31 04:52:56 +0200

    third commit

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,8 +1,8 @@
 import { Stack } from '@mui/material'
 import { categories } from '../utils/constants'
 
-const selectedCategory= 'New'
-const SideBar = () => {
+
+const SideBar = ({selectedCategory,setSelectedCategory}) => {
   return (
    <Stack 
    direction="row"
@@ -15,12 +15,22 @@ const SideBar = () => {
     {categories.map((category)=>(
         <button
         className='category-btn'
-        style={{
-            background: category.name === selectedCategory && '#FC1503', color:'white'
+        onClick={()=>{
+            setSelectedCategory(category.name)
         }}
+        style={{
+            background: category.name === selectedCategory && '#FC1503', 
+            color:'whitesmoke'
+        }}
+        key={category.name}
         >
-            <span>{category.icon} </span>
-            <span>{category.name} </span>
+            <span style={{
+                color:category.name===selectedCategory? 'white':'red',
+                marginRight:'15px'
+                }}>{category.icon} </span>
+            <span style={{
+                opacity:category.name===selectedCategory ? '1': '0.8'
+            }}>{category.name} </span>
         </button>
     ))}
    </Stack>
